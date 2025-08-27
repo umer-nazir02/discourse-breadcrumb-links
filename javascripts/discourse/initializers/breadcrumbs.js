a6bcd1b38969e53edbec8c8dd78e0255cdb91c41
umer.nazir <117436000+umer-nazir02@users.noreply.github.com>
2025-08-27 23:06:14 +0500

    add under the title

--- javascripts/discourse/initializers/breadcrumbs.js
+++ javascripts/discourse/initializers/breadcrumbs.js
@@ -3,4 +3,5 @@ import BreadCrumbs from "../components/breadcrumbs";
 
 export default apiInitializer("1.14.0", (api) => {
   api.renderInOutlet("above-main-container", BreadCrumbs);
+  api.renderInOutlet("below-topic-title", BreadCrumbs);
 });
